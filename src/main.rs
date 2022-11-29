7f67f67a0f89f600f80aea18439c03349c95a4aa
Hicham-dine Ahmed <hichamdine@gmail.com>
2022-11-30 00:44:52 +0200

    initial first commit for project kick in

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+/**
+ * this program will sniff the prots oppened for this ip address
+ *
+ * project api
+ * ip_sniffer.exe -h                    //seeking help
+ * ip_sniffer.exe -j 100 192.168.1.1    //specified threads 100
+ * ip_sniffer.exe 192.168.1.1           //specified default threads will be used
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
